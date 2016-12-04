b386582d6d091780a9cb01d3eddff6e915bd1c9b
Justin Theiss <jdeantheiss@gmail.com>
2016-12-03 22:49:10 -0800

    Again trying to remove conflict

--- code/fmri_utils/__init__.py
+++ code/fmri_utils/__init__.py
@@ -1,6 +1,4 @@
-# Init for utils package
+ # Init for utils package
 from .spm_funcs import spm_global, get_spm_globals, spm_hrf
-from .model_signal import data_timecourse, create_design_matrix, \
-    event_timecourse, hrf, beta_res_calc, create_contrast_img, \
-    compute_tstats
+from .model_signal import data_timecourse, create_design_matrix, event_timecourse, hrf, beta_res_calc, create_contrast_img, compute_tstats
 from .detectors import mean_detector, std_detector, iqr_detector
