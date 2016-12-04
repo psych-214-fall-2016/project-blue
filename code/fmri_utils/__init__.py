9f196359802735cdf1eb0b3baee00db299a5a06a
Justin Theiss <jdeantheiss@gmail.com>
2016-12-03 22:46:40 -0800

    Updated __init__.py file again to attempt to resolve conflict

--- code/fmri_utils/__init__.py
+++ code/fmri_utils/__init__.py
@@ -3,3 +3,4 @@ from .spm_funcs import spm_global, get_spm_globals, spm_hrf
 from .model_signal import data_timecourse, create_design_matrix, \
     event_timecourse, hrf, beta_res_calc, create_contrast_img, \
     compute_tstats
+from .detectors import mean_detector, std_detector, iqr_detector
