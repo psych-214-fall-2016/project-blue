3c9e3273b2d7a12b2d3d8e788e282891eed9056c
Justin Theiss <jdeantheiss@gmail.com>
2016-12-03 22:13:02 -0800

    Updated __init__.py to reflect function name changes.

--- code/fmri_utils/__init__.py
+++ code/fmri_utils/__init__.py
@@ -1,3 +1,7 @@
 # Init for utils package
 from .spm_funcs import spm_global, get_spm_globals, spm_hrf
-from .model_signal import data_timecourse, create_design_matrix, event_timecourse, hrf, b_e_calc, create_contrast_img
+from .model_signal import data_timecourse, create_design_matrix, \
+    event_timecourse, hrf, beta_res_calc, create_contrast_img, \
+    compute_tstats
+from .motion_correction import create_rotmat, transformation_matrix, \
+    apply_transform, cost_function, optimize_params
