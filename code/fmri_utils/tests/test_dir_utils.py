a444474f35c26b67a588dda189ced0d4531bc088
Justin Theiss <jdeantheiss@gmail.com>
2016-12-06 17:41:37 -0800

    Created get_contents function to be able to retrieve fields like TR from task.json file.

--- code/fmri_utils/tests/test_dir_utils.py
+++ code/fmri_utils/tests/test_dir_utils.py
@@ -6,7 +6,8 @@ Test with ``py.test test_dir_utils.py``.
 import os
 import re
 
-from fmri_utils import search_directory
+MY_DIR = os.path.dirname(__file__)
+from fmri_utils import search_directory, get_contents
 
 def test_search_directory():
     # get fullpath for filename
@@ -17,3 +18,13 @@ def test_search_directory():
     file_paths1 = search_directory(start_dir, expr)
     # assert same
     assert file_paths0 == file_paths1
+
+def test_get_contents():
+    # check that TR, TaskName, and Manufacturer are same
+    var_list = ['RepetitionTime', 'TaskName', 'Manufacturer']
+    outputs0 = [2.0, 'Visual imagery false memory', 'General Electric']
+    filename = os.path.join(MY_DIR, 'test_task.json')
+    # get outptus from filename
+    outputs1 = get_contents(filename, var_list)
+    # assert same
+    assert outputs0 == outputs1
