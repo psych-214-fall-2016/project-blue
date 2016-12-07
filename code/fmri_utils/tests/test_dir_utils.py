14865f35144ae6884db8605842115b361f7fc878
Justin Theiss <jdeantheiss@gmail.com>
2016-12-07 11:47:24 -0800

    Added validate_data/file_hash functions to dir_utils with tests. Also included data_hashes.txt for our data.

--- code/fmri_utils/tests/test_dir_utils.py
+++ code/fmri_utils/tests/test_dir_utils.py
@@ -5,9 +5,10 @@ Test with ``py.test test_dir_utils.py``.
 
 import os
 import re
+import hashlib
 
 MY_DIR = os.path.dirname(__file__)
-from fmri_utils import search_directory, get_contents
+from fmri_utils import search_directory, get_contents, file_hash, validate_data
 
 def test_search_directory():
     # get fullpath for filename
@@ -28,3 +29,32 @@ def test_get_contents():
     outputs1 = get_contents(filename, var_list)
     # assert same
     assert outputs0 == outputs1
+
+def test_file_hash():
+    # set filename to test_dir_utils
+    filename = os.path.join(MY_DIR, 'test_dir_utils.py')
+    # get contents from filename
+    fobj = open(filename, 'rb')
+    contents = fobj.read()
+    fobj.close()
+    # get hash of file
+    hash0 = hashlib.sha1(contents).hexdigest()
+    # get hash with file_hash
+    hash1 = file_hash(filename)
+    # assert same
+    assert hash0 == hash1
+
+def test_validate_hash():
+    # get data_hashes.txt
+    filename = os.path.join(MY_DIR, 'data_hashes.txt')
+    # get hashes and files
+    fobj = open(filename, 'rt')
+    lines = fobj.readlines()
+    fobj.close()
+    split_lines = [line.split() for line in lines]
+    # check hashes are same as file hashes
+    for line in split_lines:
+        fhash = file_hash(MY_DIR + '/' + line[1])
+        assert fhash == line[0]
+    # run validate_data
+    validate_data(MY_DIR)
