bd9932a5e5e64eae0394e755f8a0054ad6496177
Joe Winer <jwiner@berkeley.edu>
2016-12-10 18:08:38 -0800

    Adding problematic slice_timing update

--- code/fmri_utils/slice_timing_corr.py
+++ code/fmri_utils/slice_timing_corr.py
@@ -1,15 +1,16 @@
 """
-The slice timing code will reslice a 4D image using the scipy
+The slice timing code reslices a 4D image using the scipy
 'InterpolatedUnivariateSpline' function.
+
+This function uses the linear interpolation formula:
+y = y0 + (x - x0) * (y1 - y0) / (x1 - x0)
 
 INPUTS:
 1) functional data that has gone through outlier detection
 2) TR from scanning
-3) other scanner info?
 
-OUTPUTS:
+OUTPUT:
 1) new_data: Resliced, time corrected data
-2) new_series: Corrected time series
 """
 
 import numpy as np
@@ -22,29 +23,17 @@ def slice_timing_corr(data, TR):
     #plt.rcParams['image.cmap'] = 'gray'  # default gray colormap
     #plt.rcParams['image.interpolation'] = 'nearest'
 
+    #import timing info from openfmri json file
+    #outputs = get_contents('task-visualimageryfalsememory_bold.json', ['SliceTiming'])
+    #time_offsets = outputs[0]
+    time_offsets = [0.08, 1.08, 0.15, 1.15, 0.23, 1.23, 0.31, 1.31, 0.38, 1.38, 0.46, 1.46, 0.54, 1.54, 0.62, 1.62, 0.69, 1.69, 0.77, 1.77, 0.85, 1.85, 0.92, 1.92, 1.00, 2.00],
+
     #get timing info
     vol_nos = np.arange(data.shape[-1])
     vol_onset_times = vol_nos * TR
-    times_slice_0 = vol_onset_times
     n_z_slices = data.shape[2]
     time_for_single_slice = TR / n_z_slices
-    time_course_slice_0 = data[:, :, 0, :]
     time_course_z = data[:, :, 1:n_z_slices, :]
-
-    """
-    Need to check that the below numbers are being generated correctly
-    """
-    acquisition_order = np.zeros(n_z_slices)
-    acquisition_index = 0
-    for i in range(0, n_z_slices, 2):
-        acquisition_order[i] = acquisition_index
-        acquisition_index += 1
-    for i in range(1, n_z_slices, 2):
-        acquisition_order[i] = acquisition_index
-        acquisition_index += 1
-
-    #- Divide acquisition_order by number of slices, multiply by TR
-    time_offsets = acquisition_order / n_z_slices * TR
 
     #Copy old data into new array
     new_data = data.copy()
@@ -61,13 +50,19 @@ def slice_timing_corr(data, TR):
     #### resample this interpolator at the slice 0 times;
     #### put this new resampled time series into the new data at the
     #    same position
+
+    #For each slice (z)
     for z in range(data.shape[2]):
-        slice_z_times = times_slice_0 + time_offsets[z]
+        #Calculate the time series of acquisition
+        slice_z_times = np.array(time_offsets) + (TR * z)
+        #For each x coordinate
         for x in range(data.shape[0]):
+            #For each y coordinate
             for y in range(data.shape[1]):
+                #Get the time series at this x y z coordinate
                 time_series = data[x, y, z, :]
                 interp = InterpolatedUnivariateSpline(slice_z_times, time_series, k=1)
                 new_series = interp(times_slice_0)
                 new_data[x, y, z, :] = new_series
 
-    return new_data, new_series
+    return new_data
